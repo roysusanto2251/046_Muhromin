c419d7cbbfe8c99df298fcadbb145671762fbb96
roy <roysusanto2251@gmail.com>
2025-10-02 12:58:13 +0700

    loop

---
+++ main.py
@@ -0,0 +1,10 @@
+# Perulangan dengan for
+for i in range(5):
+    print("Perulangan ke-", i)
+
+# Perulangan dengan while
+count = 0
+while count < 5:
+    print("Perulangan while ke-", count)
+    count += 1
+
